03a4199626f0ed87aa62043a0d860b79d3b0d76c
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2025-04-24 14:32:57 +0700

    refactor: update http client dependencies (#1566)
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- tests/performance-tests/agent-performance-tests-k6/src/tests/dids/create-prism-did-test.ts
+++ tests/performance-tests/agent-performance-tests-k6/src/tests/dids/create-prism-did-test.ts
@@ -1,20 +1,20 @@
-import { Options } from 'k6/options';
-import { Issuer } from '../../actors';
-import { defaultOptions } from "../../scenarios/default";
-import merge from "ts-deepmerge";
-import { describe } from "../../k6chaijs.js";
+import { Options } from 'k6/options'
+import { Issuer } from '../../actors'
+import { defaultOptions } from '../../scenarios/default'
+import merge from 'ts-deepmerge'
+import { describe } from '../../k6chaijs.js'
 
 export const localOptions: Options = {
   thresholds: {
     'group_duration{group:::Issuer create unpublished DID}': ['avg < 5000']
   }
 }
-export let options: Options = merge(localOptions, defaultOptions)
+export const options: Options = merge(localOptions, defaultOptions)
 
-const issuer = new Issuer();
+const issuer = new Issuer()
 
 export default () => {
-  describe("Issuer create unpublished DID", function () {
-    issuer.createUnpublishedDid();
-  });
-};
+  describe('Issuer create unpublished DID', function () {
+    issuer.createUnpublishedDid()
+  })
+}
