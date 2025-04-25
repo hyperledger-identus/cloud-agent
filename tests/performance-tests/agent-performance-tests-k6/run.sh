6c4690617bf41d880c74654ce50b519366968783
Yummy-Yums <eliasderby90@gmail.com>
2025-04-25 05:29:29 +0000

    refactor: download appropriate binary for k6 perf test(#1564) (#1565)
    Signed-off-by: Yummy-Yums <eliasderby90@gmail.com>
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
    Co-authored-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
    Signed-off-by: Pat Losoponkul <pat.losoponkul@iohk.io>

--- tests/performance-tests/agent-performance-tests-k6/run.sh
+++ tests/performance-tests/agent-performance-tests-k6/run.sh
@@ -7,8 +7,12 @@ ENV_FILE=".env"
 PERF_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 AGENT_DIR="$PERF_DIR/../../.."
 DOCKERFILE="$AGENT_DIR/infrastructure/shared/docker-compose-combined.yml"
-K6_URL="https://github.com/grafana/k6/releases/download/v0.58.0/k6-v0.58.0-macos-arm64.zip"
-K6_ZIP_FILE="$(basename ${K6_URL})"
+
+K6_BASE_URL="https://github.com/grafana/k6/releases/download/v0.45.0/k6-v0.45.0-"
+K6_LINUX_ZIP_FILE="linux-amd64.tar.gz"
+K6_MACOS_INTEL_ZIP_FILE="macos-amd64.zip"
+K6_MACOS_ARM_ZIP_FILE="macos-arm64.zip"
+K6_WINDOWS_ZIP_FILE="windows-amd64.zip"
 
 # Functions
 function startAllAgents() {
@@ -24,9 +28,9 @@ function startAllAgents() {
 	local API_KEY_AUTO_PROVISIONING=false
 	local API_KEY_ENABLED=true
 
-	echo "Issuer Port [$ISSUER_PORT]"
-	echo "Holder Port [$HOLDER_PORT]"
-	echo "VerifierPort [$VERIFIER_PORT]"
+	echo "Issuer Port   [$ISSUER_PORT]"
+	echo "Holder Port   [$HOLDER_PORT]"
+	echo "Verifier Port [$VERIFIER_PORT]"
 
 	ADMIN_TOKEN="${ADMIN_TOKEN}" \
 		DEFAULT_WALLET_ENABLED="${DEFAULT_WALLET_ENABLED}" \
@@ -39,7 +43,11 @@ function startAllAgents() {
 		NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD="${NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD}" \
 		NODE_WALLET_MAX_TPS="${NODE_WALLET_MAX_TPS}" \
 		docker compose -f "${DOCKERFILE}" \
-		--env-file "${ENV_FILE}" up -d --wait 2>/dev/null
+		--env-file "${ENV_FILE}" up -d --wait 2>/dev/null || {
+		  echo "Docker failed to start? Please check whether Docker daemon is running" >&2
+		  exit 1
+		}
+	echo "starting containers in docker"
 	echo "Agents healthy"
 }
 
@@ -67,12 +75,76 @@ function cleanup() {
 	echo "cleanup complete"
 }
 
+function check_os_name_and_arch(){
+
+  if [ "$(uname -s)" = "Darwin" ]; then
+      ARCH=$(uname -m)
+      if [ "$ARCH" = "x86_64" ]; then
+          echo "MacOS $ARCH"
+      elif [ "$ARCH" = "arm64" ]; then
+          echo "MacOS $ARCH"
+      fi
+  elif [ "$(uname -s)" = "Linux" ]; then
+      ARCH=$(uname -m)
+      echo "Linux $ARCH"
+  else
+      if command -v cmd.exe &>/dev/null ||
+         command -v powershell.exe &>/dev/null ||
+         command -v systeminfo &>/dev/null; then
+         echo "WINDOWS"
+      else
+         echo "Unknown platform"
+      fi
+  fi
+
+}
+
 trap 'cleanup' EXIT
 
 # download and unzip k6
 echo "Downloading K6"
-curl -LO -s "${K6_URL}"
-unzip -j "${K6_ZIP_FILE}" >/dev/null
+FULL_ARCH=$(check_os_name_and_arch)
+
+case "$FULL_ARCH" in
+    "MacOS x86_64")
+          FULL_URL="${K6_BASE_URL}${K6_MACOS_INTEL_ZIP_FILE}"
+          ZIP_FILE=$(basename ${FULL_URL})
+          echo "Downloading for MacOS x86_64"
+          echo "Downloading from url:${FULL_URL}"
+          curl -LO -s "${FULL_URL}"
+          unzip -j "${ZIP_FILE}" >/dev/null
+        ;;
+    "MacOS arm64")
+          FULL_URL="${K6_BASE_URL}${K6_MACOS_ARM_ZIP_FILE}"
+          ZIP_FILE=$(basename ${FULL_URL})
+          echo "Downloading for MacOS arm64"
+          echo "Downloading from url:${FULL_URL}"
+          curl -LO -s "${FULL_URL}"
+          unzip -j "${ZIP_FILE}" >/dev/null
+        ;;
+    "Linux x86_64")
+          FULL_URL="${K6_BASE_URL}${K6_LINUX_ZIP_FILE}"
+          ZIP_FILE=$(basename ${FULL_URL})
+          echo "Downloading for Arch Linux x86_64"
+          echo "Downloading from url: ${FULL_URL}"
+          curl -LO -s "${FULL_URL}"
+          echo "${ZIP_FILE}"
+          tar -xzvf "${ZIP_FILE}" --strip-components=1 -C ./ --wildcards '*/k6' 2>/dev/null
+        ;;
+    "WINDOWS")
+          FULL_URL="${K6_BASE_URL}${K6_WINDOWS_ZIP_FILE}"
+          ZIP_FILE=$(basename ${FULL_URL})
+          echo "Downloading for Windows"
+          echo "Downloading from url: ${FULL_URL}"
+          curl -LO -s "${FULL_URL}"
+          tar -xf %ZIP_FILE% --strip-components=1 -C . --wildcards "*/k6.exe"
+        ;;
+    *)
+          echo "Unrecognized system: $FULL_ARCH, go to the k6 release page and download the respective binary https://github.com/grafana/k6/releases"
+          exit 1
+        ;;
+esac
+
 echo "K6 downloaded"
 
 ## navigate to main project
