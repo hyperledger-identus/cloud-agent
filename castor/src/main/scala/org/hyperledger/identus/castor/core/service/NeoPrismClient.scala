ea5326e964ded932c9a61f230a8d01f8811d6e05
Pat Losoponkul <pat.losoponkul@iohk.io>
2025-12-19 16:13:57 +0700

    Extract NeoPrismClient from NeoPrismDIDService for separation of concerns
    Signed-off-by: Pat Losoponkul <pat.losoponkul@iohk.io>

---
+++ castor/src/main/scala/org/hyperledger/identus/castor/core/service/NeoPrismClient.scala
@@ -0,0 +1,148 @@
+package org.hyperledger.identus.castor.core.service
+
+import io.iohk.atala.prism.protos.node_models
+import org.hyperledger.identus.castor.core.model.did.{CanonicalPrismDID, DIDMetadata, PrismDID}
+import org.hyperledger.identus.shared.models.HexString
+import zio.*
+import zio.http.*
+import zio.json.{DecoderOps, DeriveJsonDecoder, JsonDecoder}
+
+import java.time.Instant
+import scala.collection.immutable.ArraySeq
+
+trait NeoPrismClient {
+
+  /** Get resolution metadata for a DID.
+    * @return
+    *   None if DID not found (404), Some if found, throws on other errors
+    */
+  def getResolutionMetadata(did: PrismDID): Task[Option[DIDMetadata]]
+
+  /** Get DID data as protobuf.
+    * @return
+    *   None if DID data not found (404), Some if found, throws on other errors
+    */
+  def getDIDData(did: PrismDID): Task[Option[node_models.DIDData]]
+}
+
+private class NeoPrismClientImpl(client: Client) extends NeoPrismClient {
+  import NeoPrismClientImpl.*
+
+  private val baseClient = client.host("localhost").port(18080) // TODO: expose this configuration
+
+  override def getResolutionMetadata(did: PrismDID): Task[Option[DIDMetadata]] =
+    for
+      resp <- baseClient.batched
+        .addHeader("Content-Type", "application/did-resolution")
+        .get(s"api/dids/$did")
+      metadataOpt <- resp.status match
+        case Status.NotFound => ZIO.none
+        case _ =>
+          resp.body.asString
+            .flatMap { body =>
+              ZIO
+                .fromEither(body.fromJson[NeoPrismResolutionResult])
+                .mapError(e => new RuntimeException(s"Failed to decode JSON: $e"))
+                .map(result => Some(convertToMetadata(result.didDocumentMetadata)))
+            }
+    yield metadataOpt
+
+  override def getDIDData(did: PrismDID): Task[Option[node_models.DIDData]] =
+    for
+      didDataResp <- baseClient.batched
+        .get(s"api/did-data/$did")
+      protoDIDDataOpt <- didDataResp.status match
+        case Status.Ok =>
+          for
+            hexStr <- didDataResp.body.asString
+            hexBytes <- ZIO.fromTry(HexString.fromString(hexStr))
+            protoData <- ZIO.attempt(node_models.DIDData.parseFrom(hexBytes.toByteArray))
+          yield Some(protoData)
+        case Status.NotFound =>
+          ZIO.succeed(None)
+        case status =>
+          ZIO.fail(new RuntimeException(s"Unexpected status code from did-data: ${status.code}"))
+    yield protoDIDDataOpt
+
+  private def convertToMetadata(documentMetadata: NeoPrismDocumentMetadata): DIDMetadata = {
+    val lastOperationHash = documentMetadata.versionId
+      .flatMap(v => HexString.fromString(v).toOption)
+      .map(h => ArraySeq.unsafeWrapArray(h.toByteArray))
+      .getOrElse(ArraySeq.empty[Byte])
+
+    val canonicalId: Option[CanonicalPrismDID] = documentMetadata.canonicalId.flatMap { cidStr =>
+      PrismDID.fromString(cidStr).toOption.map(_.asCanonical)
+    }
+
+    val created: Option[Instant] = documentMetadata.created.flatMap { createdStr =>
+      scala.util.Try(Instant.parse(createdStr)).toOption
+    }
+
+    val updated: Option[Instant] = documentMetadata.updated.flatMap { updatedStr =>
+      scala.util.Try(Instant.parse(updatedStr)).toOption
+    }
+
+    DIDMetadata(
+      lastOperationHash = lastOperationHash,
+      canonicalId = canonicalId,
+      deactivated = documentMetadata.deactivated.getOrElse(false),
+      created = created,
+      updated = updated
+    )
+  }
+}
+
+object NeoPrismClientImpl {
+  val layer: URLayer[Client, NeoPrismClient] =
+    ZLayer.fromFunction(NeoPrismClientImpl(_))
+
+  // JSON response models from NeoPRISM API - private to implementation
+  private case class NeoPrismResolutionResult(
+      didResolutionMetadata: NeoPrismResolutionMetadata,
+      didDocumentMetadata: NeoPrismDocumentMetadata,
+      didDocument: Option[NeoPrismDidDocument]
+  )
+
+  private object NeoPrismResolutionResult {
+    given decoder: JsonDecoder[NeoPrismResolutionResult] = DeriveJsonDecoder.gen
+  }
+
+  private case class NeoPrismResolutionMetadata(
+      contentType: Option[String],
+      error: Option[NeoPrismResolutionError]
+  )
+
+  private object NeoPrismResolutionMetadata {
+    given decoder: JsonDecoder[NeoPrismResolutionMetadata] = DeriveJsonDecoder.gen
+  }
+
+  private case class NeoPrismResolutionError(
+      `type`: String,
+      title: Option[String],
+      detail: Option[String]
+  )
+
+  private object NeoPrismResolutionError {
+    given decoder: JsonDecoder[NeoPrismResolutionError] = DeriveJsonDecoder.gen
+  }
+
+  private case class NeoPrismDocumentMetadata(
+      canonicalId: Option[String],
+      created: Option[String],
+      deactivated: Option[Boolean],
+      updated: Option[String],
+      versionId: Option[String]
+  )
+
+  private object NeoPrismDocumentMetadata {
+    given decoder: JsonDecoder[NeoPrismDocumentMetadata] = DeriveJsonDecoder.gen
+  }
+
+  private case class NeoPrismDidDocument(
+      id: String
+  )
+
+  private object NeoPrismDidDocument {
+    given decoder: JsonDecoder[NeoPrismDidDocument] = DeriveJsonDecoder.gen
+  }
+}
--- castor/src/main/scala/org/hyperledger/identus/castor/core/service/NeoPrismDIDService.scala
+++ castor/src/main/scala/org/hyperledger/identus/castor/core/service/NeoPrismDIDService.scala
@@ -2,7 +2,6 @@ package org.hyperledger.identus.castor.core.service
 
 import io.iohk.atala.prism.protos.node_models
 import org.hyperledger.identus.castor.core.model.did.{
-  CanonicalPrismDID,
   DIDData,
   DIDMetadata,
   PrismDID,
@@ -12,73 +11,14 @@ import org.hyperledger.identus.castor.core.model.did.{
 }
 import org.hyperledger.identus.castor.core.model.error.{DIDOperationError, DIDResolutionError}
 import org.hyperledger.identus.castor.core.model.ProtoModelHelper.*
-import org.hyperledger.identus.shared.models.HexString
 import zio.*
-import zio.http.*
-import zio.json.{DecoderOps, DeriveJsonDecoder, JsonDecoder}
-
-import java.time.Instant
-import scala.collection.immutable.ArraySeq
 
 object NeoPrismDIDService {
-  val layer: URLayer[Client, DIDService] =
+  val layer: URLayer[NeoPrismClient, DIDService] =
     ZLayer.fromFunction(NeoPrismDIDService(_))
-
-  // JSON response models from NeoPRISM API
-  private case class NeoPrismResolutionResult(
-      didResolutionMetadata: NeoPrismResolutionMetadata,
-      didDocumentMetadata: NeoPrismDocumentMetadata,
-      didDocument: Option[NeoPrismDidDocument]
-  )
-
-  private object NeoPrismResolutionResult {
-    given decoder: JsonDecoder[NeoPrismResolutionResult] = DeriveJsonDecoder.gen[NeoPrismResolutionResult]
-  }
-
-  private case class NeoPrismResolutionMetadata(
-      contentType: Option[String],
-      error: Option[NeoPrismResolutionError]
-  )
-
-  private object NeoPrismResolutionMetadata {
-    given decoder: JsonDecoder[NeoPrismResolutionMetadata] = DeriveJsonDecoder.gen[NeoPrismResolutionMetadata]
-  }
-
-  private case class NeoPrismResolutionError(
-      `type`: String,
-      title: Option[String],
-      detail: Option[String]
-  )
-
-  private object NeoPrismResolutionError {
-    given decoder: JsonDecoder[NeoPrismResolutionError] = DeriveJsonDecoder.gen[NeoPrismResolutionError]
-  }
-
-  private case class NeoPrismDocumentMetadata(
-      canonicalId: Option[String],
-      created: Option[String],
-      deactivated: Option[Boolean],
-      updated: Option[String],
-      versionId: Option[String]
-  )
-
-  private object NeoPrismDocumentMetadata {
-    given decoder: JsonDecoder[NeoPrismDocumentMetadata] = DeriveJsonDecoder.gen[NeoPrismDocumentMetadata]
-  }
-
-  private case class NeoPrismDidDocument(
-      id: String
-  )
-
-  private object NeoPrismDidDocument {
-    given decoder: JsonDecoder[NeoPrismDidDocument] = DeriveJsonDecoder.gen[NeoPrismDidDocument]
-  }
 }
 
-private class NeoPrismDIDService(client: Client) extends DIDService:
-  import NeoPrismDIDService.*
-
-  private val baseClient = client.host("localhost").port(18080)
+private class NeoPrismDIDService(client: NeoPrismClient) extends DIDService:
 
   override def scheduleOperation(
       operation: SignedPrismDIDOperation
@@ -90,95 +30,34 @@ private class NeoPrismDIDService(client: Client) extends DIDService:
 
   override def resolveDID(did: PrismDID): IO[DIDResolutionError, Option[(DIDMetadata, DIDData)]] =
     for
-      // Step 1: Call GET /api/dids/{did} to get resolution metadata
-      resp <- baseClient.batched
-        .addHeader("Content-Type", "application/did-resolution")
-        .get(s"api/dids/$did")
+      // Call client to get resolution metadata (already as DIDMetadata)
+      metadataOpt <- client
+        .getResolutionMetadata(did)
         .mapError(ex => DIDResolutionError.DLTProxyError("Error resolving DID document from NeoPRISM", ex))
 
-      // Step 2: Handle response status codes
-      resolutionResultOpt <- resp.status match
-        case Status.Ok =>
-          resp.body.asString
-            .mapError(t => DIDResolutionError.DLTProxyError("Failed to parse response body", t))
-            .flatMap { body =>
-              ZIO
-                .fromEither(body.fromJson[NeoPrismResolutionResult])
-                .mapError(e => DIDResolutionError.DLTProxyError(s"Failed to decode JSON: $e", null))
-                .map(Some(_))
-            }
-        case Status.NotFound | Status.BadRequest =>
-          ZIO.succeed(None)
-        case status =>
-          ZIO.fail(DIDResolutionError.DLTProxyError(s"Unexpected status code: ${status.code}", null))
-
-      // Step 3: If DID not found, return None early
-      result <- resolutionResultOpt match
+      // If DID not found, return None early
+      result <- metadataOpt match
         case None => ZIO.succeed(None)
-        case Some(resolutionResult) =>
+        case Some(metadata) =>
           for
-            // Step 4: Call GET /api/did-data/{did} to get protobuf DIDData
-            didDataResp <- baseClient.batched
-              .get(s"api/did-data/$did")
+            // Call client to get protobuf DIDData
+            protoDIDDataOpt <- client
+              .getDIDData(did)
               .mapError(ex => DIDResolutionError.DLTProxyError("Error fetching DIDData from NeoPRISM", ex))
 
-            // Step 5: Parse hex-encoded protobuf
-            protoDIDData: node_models.DIDData <- didDataResp.status match
-              case Status.Ok =>
-                for
-                  hexStr <- didDataResp.body.asString
-                    .mapError(t => DIDResolutionError.DLTProxyError("Failed to read DIDData response body", t))
-                  hexBytes <- ZIO
-                    .fromTry(HexString.fromString(hexStr))
-                    .mapError(e => DIDResolutionError.DLTProxyError(s"Invalid hex string: ${e.getMessage}", e))
-                  protoData <- ZIO
-                    .attempt(node_models.DIDData.parseFrom(hexBytes.toByteArray))
-                    .mapError(t => DIDResolutionError.DLTProxyError("Failed to parse protobuf", t))
-                yield protoData
-              case Status.NotFound =>
+            // Handle case where resolution succeeded but DID data is missing
+            protoDIDData <- protoDIDDataOpt match
+              case Some(data) => ZIO.succeed(data)
+              case None =>
                 ZIO.fail(
                   DIDResolutionError.UnexpectedDLTResult("DIDData not found but resolution metadata succeeded")
                 )
-              case status =>
-                ZIO.fail(
-                  DIDResolutionError.DLTProxyError(s"Unexpected status code from did-data: ${status.code}", null)
-                )
 
-            // Step 6: Convert protobuf to domain model
+            // Convert protobuf to domain model
             didData <- ZIO
               .fromEither(protoDIDData.toDomain)
               .mapError(e =>
                 DIDResolutionError.UnexpectedDLTResult(s"Failed to convert protobuf DIDData to domain model: $e")
               )
-
-            // Step 7: Convert NeoPRISM metadata to domain model
-            metadata = convertToMetadata(resolutionResult.didDocumentMetadata)
           yield Some((metadata, didData))
     yield result
-
-  private def convertToMetadata(documentMetadata: NeoPrismDocumentMetadata): DIDMetadata = {
-    val lastOperationHash = documentMetadata.versionId
-      .flatMap(v => HexString.fromString(v).toOption)
-      .map(h => ArraySeq.unsafeWrapArray(h.toByteArray))
-      .getOrElse(ArraySeq.empty[Byte])
-
-    val canonicalId: Option[CanonicalPrismDID] = documentMetadata.canonicalId.flatMap { cidStr =>
-      PrismDID.fromString(cidStr).toOption.map(_.asCanonical)
-    }
-
-    val created: Option[Instant] = documentMetadata.created.flatMap { createdStr =>
-      scala.util.Try(Instant.parse(createdStr)).toOption
-    }
-
-    val updated: Option[Instant] = documentMetadata.updated.flatMap { updatedStr =>
-      scala.util.Try(Instant.parse(updatedStr)).toOption
-    }
-
-    DIDMetadata(
-      lastOperationHash = lastOperationHash,
-      canonicalId = canonicalId,
-      deactivated = documentMetadata.deactivated.getOrElse(false),
-      created = created,
-      updated = updated
-    )
-  }
